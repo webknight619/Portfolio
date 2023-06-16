a61b16acd60a166d20374756bad38ff92afe8247
Kunal <kunalsuck4u@gmail.com>
2023-06-16 13:19:31 +0530

    first commit

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,10 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: "Hello! My name is Kunal, and I'm a full stack web developer.  I love building websites and applications that work smoothly from start to finish.I have a good understanding of both the front end and the back end of web development.  On the front end, I use HTML, CSS, and JavaScript to create the parts of the website that people see and interact with.  I enjoy making things look nice and easy to use. On the back end, I work with server-side technologies like Node.js, Django, or Ruby on Rails. These help me handle the behind-the-scenes tasks that make websites work properly.  I also know how to work with databases, like MySQL, PostgreSQL, or MongoDB, to store and retrieve data efficiently. I use frameworks like React or Angular to connect the front end and the back end, making sure everything works together smoothly.  I believe in working well with others and being a team player. I like collaborating with designers, other developers, and the people who need the website or application. Clear communication is important to me, so everyone is on the same page throughout the development process.",
+	},
+	{
+		id: 2,
+		bio: "When I'm not coding, I like to keep up with the latest happenings in web development.  I enjoy learning about new technologies and frameworks to improve my skills and stay up-to-date.If you need a dedicated and versatile full stack web developer, I'm here to help. Let's connect and discuss how we can work together to create outstanding websites and applications that make a difference.",
+	},
+];
